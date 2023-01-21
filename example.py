ba1334668b2452947cd3e6bc6fae4871363037f4
Manu V Nair <manu@synthara.ai>
2023-01-21 23:48:51 +0100

    Change file names to indicate they are examples

--- example.py
+++ example.py
@@ -1,10 +1,10 @@
 from StatsDB import StatsDB
 
 if __name__ == "__main__":
-    db = StatsDB("vars.csv")
+    db = StatsDB("ex_db.csv")
     db.print()
     db.query("Op.Hz", "#CX")
     db.query("Op.Hz", "#MVM")
     db.query("J/pkt", "#PAP")
     # db.query("W/Op")
-    db.write_db_to_csv("op.csv")
+    db.write_db_to_csv("ex_op_db.csv")
